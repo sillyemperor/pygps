05415fe02a17c5d065004bd6cf66e54cb565f364
peter <sillyemperor@qq.com>
2017-09-22 02:17:20 +0800

    增加server组建

--- protocols/server.py
+++ protocols/server.py
@@ -1,13 +1,12 @@
 # -*- coding:utf8 -*-
 import SocketServer
-import socket
-import threading
+import binascii
 
 
 class ProtocolTCPRequestHandler(SocketServer.BaseRequestHandler):
     def handle(self):
         data = self.request.recv(1024)
-        print data
+        print binascii.b2a_hex(data), data
         self.request.sendall(data)
 
 
@@ -15,12 +14,12 @@ class ProtocolUDPHandler(SocketServer.BaseRequestHandler):
     def handle(self):
         data = self.request[0].strip()
         socket = self.request[1]
-        print data
+        print binascii.b2a_hex(data), data
         socket.sendto(data, self.client_address)
 
 
 # server = SocketServer.ThreadingTCPServer(('', 3007), ProtocolTCPRequestHandler)
-server = SocketServer.ThreadingUDPServer(('', 3007), ProtocolTCPRequestHandler)
+server = SocketServer.ThreadingUDPServer(('', 3007), ProtocolUDPHandler)
 server.serve_forever()
 
 
