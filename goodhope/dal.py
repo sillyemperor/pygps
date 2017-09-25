3e02a40d61f0db7877c5295320be9391fafdb798
peter <sillyemperor@qq.com>
2017-09-25 20:36:15 +0800

    好望角数据层

--- goodhope/dal.py
+++ goodhope/dal.py
@@ -1,2 +1,32 @@
 # -*- coding:utf8 -*-
+import pyodbc
+import Queue
+
+class GPSDal:
+    def __init__(self, db_connections):
+        self.location_buff = Queue.Queue(1024)
+        self.db_connections = db_connections
+
+    def fetchall(self, sql, *args):
+        with pyodbc.connect(self.db_connections) as conn:
+            with conn.cursor() as cur:
+                cur.execute(sql, *args)
+                return cur.fetchall()
+
+    def fetchone(self, sql, *args):
+        with pyodbc.connect(self.db_connections) as conn:
+            with conn.cursor() as cur:
+                cur.execute(sql, *args)
+                return cur.fetchone()
+
+    def execute(self, sql, *args):
+        with pyodbc.connect(self.db_connections) as conn:
+            with conn.cursor() as cur:
+                cur.execute(sql, *args)
+                cur.commit()
+                return cur.fetchone()
+
+    def add_location(self, location):
+        pass
+
 
