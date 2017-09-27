683bbbbb1b8d694bda6534e2d34ed08b5a091109
peter <sillyemperor@qq.com>
2017-09-27 23:05:19 +0800

    创建主程序入口__main__

--- pygps/server/handlers.py
+++ pygps/server/handlers.py
@@ -59,18 +59,13 @@ class ProtocalUDPHandler(protocol.DatagramProtocol):
         self.user_signal=user_signal
 
     def datagramReceived(self, data, (host, port)):
-        input_data = data
-        try:
-            result, response, input_data = self.translator.on_message(data)
-            print result, response, input_data
-            if response:
-                self.transport.write(self.translator.encode_data(response),(host, port))
-            self.pusher.push(result)
-            if self.user_signal:
-                for sid, name in self.user_signal.get_all_signal(result.imei):
-                    signal = self.translator.build_signal(name, input_data)
-                    if signal:
-                        self.transport.write(self.translator.encode_data(signal),(host, port))
-                    self.user_signal.mark_read_signal(sid)
-        except Exception as e:
-            logging.error('route fail err=%s input_data=%s ', e, input_data)
+        result, response, input_data = self.translator.on_message(data)
+        if response:
+            self.transport.write(self.translator.encode_data(response), (host, port))
+        self.pusher.push(result)
+        if self.user_signal:
+            for sid, name in self.user_signal.get_all_signal(result.imei):
+                signal = self.translator.build_signal(name, input_data)
+                if signal:
+                    self.transport.write(self.translator.encode_data(signal), (host, port))
+                self.user_signal.mark_read_signal(sid)
